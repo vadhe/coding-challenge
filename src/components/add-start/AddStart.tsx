25043aca678e2e6db76a1e31f92abe886f23f206
vadhe <aldyvadhe@gmail.com>
2021-10-10 18:01:48 +0800

    create component addStart

---
+++ src/components/add-start/AddStart.tsx
@@ -0,0 +1,17 @@
+import './AddStart.css'
+
+import * as React from 'react'
+
+import Modal from '../modal/Modal'
+
+const AddStart:React.FC = () => {
+    return (
+        <Modal>
+            <div className="add-start">
+              ini tambah review
+            </div>
+        </Modal>
+    )
+}
+
+export default AddStart
--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -1,18 +1,20 @@
-import './Start.css'
+import "./Start.css";
 
-import startnone from '../../assets/images/none.png'
-import starton from '../../assets/images/on.png'
+import * as React from "react";
 
-const Start = () => {
-    return(
-        <div className="start">
-            <img src={starton} />
-            <img src={starton} />
-            <img src={startnone} />
-            <img src={startnone} />
-            <img src={startnone} />
-        </div>
-    )
+import startnone from "../../assets/images/none.png";
+import starton from "../../assets/images/on.png";
+
+const Start: React.FC = () => {
+  return (
+    <div className="start">
+      <img alt="ok" src={starton} />
+      <img alt="ok" src={starton} />
+      <img alt="ok" src={startnone} />
+      <img alt="ok" src={startnone} />
+      <img alt="ok" src={startnone} />
+    </div>
+  );
 };
 
 export default Start;
