c9f941a8ed01fbd6589d356062582fb4b72232a9
vadhe <aldyvadhe@gmail.com>
2021-10-14 17:53:15 +0800

    create addstart component

--- src/components/add-start/AddStart.tsx
+++ src/components/add-start/AddStart.tsx
@@ -2,13 +2,21 @@ import './AddStart.css'
 
 import * as React from 'react'
 
+import Button from '../button/Button'
 import Modal from '../modal/Modal'
 
 const AddStart:React.FC = () => {
     return (
         <Modal>
             <div className="add-start">
-              ini tambah review
+              <h2>What's Your Rating ?</h2>
+              <form action="">
+                  <label htmlFor="rating">Rating</label>
+                  <input type="text" placeholder="ini bintang" />
+                  <label htmlFor="">Review</label>
+                  <input type="text" placeholder="Start typing..." />
+                  <Button label="Submit review" onClick={()=> alert("okokok")}/>
+              </form>
             </div>
         </Modal>
     )
