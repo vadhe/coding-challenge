967187bc9564596a98f28afd648798c01c444bc7
vadhe <aldyvadhe@gmail.com>
2021-10-15 20:37:39 +0800

    add function restartStar and update type StartContext

--- src/context/StarContex.tsx
+++ src/context/StarContex.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react'
 interface InterfaceStarContext{
   star: number 
-  //   setStar: React.useState(0);
-  addStar: (value: number) => void
+  addStar?: (value: number) => void
+  restartStar?: () => void
 }
-export const StarContext = React.createContext<InterfaceStarContext | null>(null);
+export const StarContext = React.createContext<InterfaceStarContext>({star:0});
 
 const RatingProvider:React.FC<React.ReactNode> = ({children}) => {
     let [star, setStar] = React.useState(0);
     const addStar = (value: number):void => {
         setStar(value)
     }
+    const restartStar = () => {
+        setStar(0)
+    }
     return (
-        <StarContext.Provider value={{star, addStar}}>
+        <StarContext.Provider value={{star, addStar, restartStar}}>
             {children}
         </StarContext.Provider>
     )
