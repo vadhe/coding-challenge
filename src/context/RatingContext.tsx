212b34cc7ada3b6152812261af7ce34f60d00a38
vadhe <aldyvadhe@gmail.com>
2021-10-15 20:38:19 +0800

    update type error RatingContext

--- src/context/RatingContext.tsx
+++ src/context/RatingContext.tsx
@@ -5,27 +5,17 @@ import { typeRatings } from '../type/types';
 
 interface InterfaceRatingContext{
     ratings: typeRatings[],
-    dispatch: any
+    dispatch?: any
 }
-export const RatingContext = React.createContext<InterfaceRatingContext | null>(null);
+export const RatingContext = React.createContext<InterfaceRatingContext>({
+    ratings: [{
+        label: "ini label",
+        star: 4
+    }],
+});
 
 const RatingProvider:React.FC<React.ReactNode> = ({children}) => {
-    const [ratings, dispatch] = React.useReducer(RatingReducer,[
-        {
-            label:"ini adalah sebuah label",
-            start:  2
-        },
-    ])
-    // const [rating, setrating] = React.useState([
-        
-        // {
-        //     label:"ini adalah sebuah label 2",
-        //     start:  5
-        // }
-    // ])
-    // const addRating = () => {
-       
-    // }
+    const [ratings, dispatch] = React.useReducer(RatingReducer,[])
     return (
         <RatingContext.Provider value={{ratings, dispatch}}>
             {children}
